807f8b24c09b113882e07c3803eab02ba4185fc6
Timotin Mihai <mihaitimotin@gmail.com>
2021-09-26 21:48:58 +0300

    create server an basic hall order

---
+++ Hall/Hall/Controllers/OrderApiController.cs
@@ -0,0 +1,25 @@
+﻿using Hall.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hall.Controllers
+{
+     [Route("[controller]")]
+     [ApiController]
+     [Produces("application/json")]
+     public class OrderApiController : Controller
+     {
+          private readonly IOrder _order;
+
+          public OrderApiController(IOrder order)
+          {
+               _order = order;
+          }
+
+          [HttpPost("Order")]
+          public IActionResult Order()
+          {
+               var response = _order.CreateOrder(1, 1, 1);
+               return Json(response);
+          }
+     }
+}
---
+++ Hall/Hall/Services/IOrder.cs
@@ -0,0 +1,13 @@
+﻿using Hall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hall.Services
+{
+     public interface IOrder
+     {
+          Order CreateOrder(int id, int tableId, int waiterId);
+     }
+}
